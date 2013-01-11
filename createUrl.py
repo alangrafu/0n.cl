550e1fda73d79980087bca9b5a18e9abccaf1bf2
alangrafu <alvaro@graves.cl>
2013-01-10 22:46:29 -0700

    mejorada la creación de ID, ahora en base al largo del archivo csv
    Pregunta: ¿Qué pasa si borro elementos => tamaño CSV diminuye?

--- createUrl.py
+++ createUrl.py
@@ -1,29 +1,44 @@
 import csv
 import sys
+import time
+
+baseUrl = 'http://0n.cl'
+def getParam( m):
+  n = m
+  s = []
+  chars =  map(unichr, range(43, 123))
+  base = len(chars)
+  if n < base:
+    return n
+  while n > 0:
+    x = n/base
+    s.append(chars[n%base])
+    n = x
+  
+  s.reverse()
+  print m
+  return 
 
 f = 'urls.csv'
 url =  sys.argv[1]
 reader = csv.reader(open(f, 'r'), delimiter=',')
 urls = {}
-
 for line in reader:
   urls[line[1]] = line[0]
 
+currentSize = len(urls)
+id = getParam(currentSize)
+
 if url in urls.keys():
-  print url, "ya existe!",urls[url]
+  id = urls[url]
 else:
   #for i in range(12737, 12751):
-  id = None
-  for i in xrange(33, 127):
-    id = unichr(int(unicode(hex(i)), 16))
-    if not id in urls.values():
-      break
   if id == None:
     print "Can't add",url,"to source"
     exit(1)
   urls[url] = id
-  print id
   writer = csv.writer(open(f, 'w'), delimiter=',')
   for i in urls:
     writer.writerow([urls[i], i])
-  
+
+print "%s/%s" %(baseUrl,id)
